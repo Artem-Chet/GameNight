155c27ded77b5b60cd9502354a9d8ac518545463
Roman Chetverikov <r_chetverikov@hotmail.com>
2023-08-11 21:44:49 -0700

    Added PlayedGame Controller

---
+++ GameNight/Server/PlayedGames/PlayedGamesController.cs
@@ -0,0 +1,37 @@
+﻿using GameNight.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameNight.Server.PlayedGames;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PlayedGamesController : ControllerBase
+{
+    [HttpGet]
+    public List<PlayedGame> Index()
+    {
+        return new List<PlayedGame>()
+        {
+            new PlayedGame()
+            {
+                GameName = "Monopoly",
+                DurationMinutes = 60,
+                StartedAtUtc = new DateTime(2023, 8, 11, 7, 0, 0, DateTimeKind.Utc),
+                Players = new()
+                {
+                    new()
+                    {
+                        Name = "Artem",
+                        IsWinner = true,
+                    },
+                    new()
+                    {
+                        Name = "Roman",
+                        IsWinner = false,
+                    }
+                }
+            }
+        };
+    }
+}
