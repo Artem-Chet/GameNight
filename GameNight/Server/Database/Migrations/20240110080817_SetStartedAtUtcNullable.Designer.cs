437dfafa7e1a73d7518d8a1f6919b5ce00673408
Artem Chetverikov <artemchetverikov@gmail.com>
2024-01-10 00:09:07 -0800

    Add missing StartedAtUtc migration and move migrations to a new folder

--- GameNight/Server/Database/Migrations/20240110080817_SetStartedAtUtcNullable.Designer.cs
+++ GameNight/Server/Database/Migrations/20240110080817_SetStartedAtUtcNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GameNight.Server.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace GameNight.Server.Migrations
 {
     [DbContext(typeof(GameContext))]
-    partial class GameContextModelSnapshot : ModelSnapshot
+    [Migration("20240110080817_SetStartedAtUtcNullable")]
+    partial class SetStartedAtUtcNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -38,7 +41,7 @@ namespace GameNight.Server.Migrations
                         .HasColumnType("text")
                         .HasColumnName("game_name");
 
-                    b.Property<DateTime>("StartedAtUtc")
+                    b.Property<DateTime?>("StartedAtUtc")
                         .HasColumnType("timestamp with time zone")
                         .HasColumnName("started_at_utc");
 
---
+++ GameNight/Server/Database/Migrations/20240110080817_SetStartedAtUtcNullable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameNight.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetStartedAtUtcNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "started_at_utc",
+                table: "played_games",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "started_at_utc",
+                table: "played_games",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+        }
+    }
+}
